231750ba2b36eba0ccad43267288c93566174b70
Misha <hryvinskyim@gmail.com>
2025-01-09 17:22:09 +0200

    styled

---
+++ src/components/App/Table/Table.styled.jsx
@@ -0,0 +1,9 @@
+import  styled  from '@emotion/styled';
+
+export const TableStyled = styled.table`
+    margin: 0 auto;
+    align-items: center;
+    width: 1800px;
+    border-collapse: collapse;
+    margin-bottom: 80px;
+`;
