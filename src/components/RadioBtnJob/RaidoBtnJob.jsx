b26a10f7da07b2acd526a9e35132cd7fc354ed43
Misha <hryvinskyim@gmail.com>
2025-01-13 00:42:32 +0200

    create RadioBtn component

---
+++ src/components/RadioBtnJob/RaidoBtnJob.jsx
@@ -0,0 +1,27 @@
+import { FilesedRadio, InputRadio, } from "components/Form/Form.styled"
+import { arrJob } from "DB/data";
+
+const RadioBtnJob = ({ radioCheck, selectedJob }) => {
+    return(
+        <FilesedRadio>
+        <legend>Оплата роботи</legend>
+            { arrJob.map(({ id, price, text }) => 
+            <label 
+                key={id} 
+                htmlFor={id}
+            >
+                <InputRadio  
+                    id={id} 
+                    name="job"
+                    type="radio" 
+                    value={price}
+                    checked={selectedJob === price}
+                    onChange={radioCheck}
+                />
+                {text}
+            </label>) }
+        </FilesedRadio>
+    )
+};
+
+export default RadioBtnJob;
