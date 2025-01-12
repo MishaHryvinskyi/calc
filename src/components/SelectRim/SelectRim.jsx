de17e04da30244ebf921efa96d779eac163d19c8
Misha <hryvinskyim@gmail.com>
2025-01-13 00:43:12 +0200

    create SelectedRim component

---
+++ src/components/SelectRim/SelectRim.jsx
@@ -0,0 +1,24 @@
+import { FieldsetStyled } from "components/Form/Form.styled";
+import { FaGlasses } from "react-icons/fa";
+import { arrRim } from "DB/data";
+
+const SelectRim = ({ checkRim, selectedRim }) => {
+    return (
+        <FieldsetStyled>
+            <legend><FaGlasses /> Оправи</legend>
+                <select 
+                    value={selectedRim} 
+                    onChange={checkRim}
+                >
+                    <option value='default' disabled>Виберіть оправу</option>
+                        {arrRim.map((item) => (
+                            <option key={item} value={item}>
+                                {item} грн
+                            </option>
+                        ))}
+                </select>
+        </FieldsetStyled>
+    )
+};
+
+export default SelectRim;
