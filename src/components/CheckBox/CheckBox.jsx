c80f7da8f49cd727cfb86772bd7febf26d913d4f
Misha <hryvinskyim@gmail.com>
2025-01-13 00:41:35 +0200

    cut out the checkboxes from the form

---
+++ src/components/CheckBox/CheckBox.jsx
@@ -0,0 +1,32 @@
+import { FilesedCheckBox } from "components/Form/Form.styled"
+import { IoGlasses } from "react-icons/io5";
+import { TbUrgent } from "react-icons/tb";
+
+const CheckBox = ({ checkTon, checkVarn, ton, varn }) => {
+    return (
+        <FilesedCheckBox>
+            <legend>Додаткові опції</legend>
+                <label>
+                    <input 
+                        type="checkbox" 
+                        name="ton" 
+                        value="250"
+                        checked={ton}
+                        onChange={checkTon}
+                    /> Тонування <IoGlasses/>
+                </label>
+            
+                <label>
+                    <input 
+                         type="checkbox"
+                         name="urgency" 
+                         value="orange"
+                         checked={varn}
+                         onChange={checkVarn}
+                    /> Терміновість <TbUrgent/>
+                </label>
+        </FilesedCheckBox>
+    )
+};
+
+export default CheckBox;
