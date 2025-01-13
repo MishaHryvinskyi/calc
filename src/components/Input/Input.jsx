8a3f5312308cde86bb7fcbeac167e7274574d8e8
Misha <hryvinskyim@gmail.com>
2025-01-13 18:09:37 +0200

    added Input component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,28 @@
+import { InputStyled, LabelStyled } from "components/Form/Form.styled";
+
+const Input = ({ 
+    handleClick,
+    placeholder,
+    userName,
+    pattern, 
+    text, 
+    name, 
+    type, 
+}) => {
+    return (
+        <LabelStyled>
+            {text}
+            <InputStyled 
+                required 
+                name={name} 
+                placeholder={placeholder}
+                value={userName}
+                onChange={handleClick}
+                pattern={pattern}
+                type={type}
+            />
+        </LabelStyled>
+    )
+};
+
+export default Input;
