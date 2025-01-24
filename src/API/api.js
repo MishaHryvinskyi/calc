57f6ef9475f88849130b12e17bfb97a2619771ee
Misha <hryvinskyim@gmail.com>
2025-01-24 15:17:19 +0200

    added async and axios func

--- src/API/api.js
+++ src/API/api.js
@@ -1,41 +1,28 @@
-const API = 'https://6776676812a55a9a7d0b9792.mockapi.io/orders';
+import axios from "axios";
 
+const API = 'https://6776676812a55a9a7d0b9792.mockapi.io/orders';
 const WATHER_URL = 'https://api.weatherapi.com/v1';
 const WATHER_API_KEY = 'c0dca8ed73ba444e8db231919241301';
+const WEATHER_API = `${WATHER_URL}/forecast.json?key=${WATHER_API_KEY}&q=kosiv&days=1&lang=uk`;
 
-const getUsers = () => {
-    return fetch(API)
-    .then(res => res.json())
-}
+const getUsers = async () => {
+    const allUsers = await axios.get(API);
+    return allUsers;
+};
 
-const createUsers = (value) => {
-    const options = {
-        method: "POST",
-        body: JSON.stringify(value),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }
-    return fetch(API, options).then(res => res.json())
-}
+const createUsers = async (newUser) => {
+    const createUser = await axios.post(API, newUser)
+    return createUser;
+};
 
-const removeUser = (id) => {
-    const options = {
-        method: "DELETE",
-    }
+const removeUser = async (id) => {
+    const deleteUser = await axios.delete(`${API}/${id}`);
+    return deleteUser;
+};
 
-    return fetch(`${API}/${id}`, options).then(res => res.json())
-}
-
-const getWeather = () => {
-    return fetch(`${WATHER_URL}/forecast.json?key=${WATHER_API_KEY}&q=kosiv&days=1&lang=uk`)
-    .then(response => {
-        if(!response.ok) {
-            throw new Error(response.statusText);
-        }
-
-        return response.json();
-    })
+const getWeather = async () => {
+    const weather = await axios.get(WEATHER_API);
+    return weather;
 }
 
 export {
