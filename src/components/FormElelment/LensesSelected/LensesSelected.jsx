687aa33fc511cf217ba81e6a98791bab2f7c8728
Misha <hryvinskyim@gmail.com>
2025-02-11 20:07:13 +0200

    fix

--- src/components/FormElelment/LensesSelected/LensesSelected.jsx
+++ src/components/FormElelment/LensesSelected/LensesSelected.jsx
@@ -5,16 +5,8 @@ const LensesSelected = ({ checkLens, lenses }) => {
     return (
         <FieldsetStyled>
             <legend>Лінзи</legend>
-            <select 
-            name="lenses"  
-            onChange={checkLens}
-            value={lenses}
-            >
-            <option 
-                value='default' 
-                disabled
-            >Виберіть лінзи згідно рецепту
-            </option>
+            <select name="lenses" onChange={checkLens} value={lenses}>
+            <option value='default' disabled>Виберіть лінзи згідно рецепту</option>
                 <optgroup label="Литва">
                     {lensenPriceLitva.map(({ lensenName, price }) => 
                         <option key={lensenName} value={price}>{`${lensenName} ${price}`} грн/шт</option>
